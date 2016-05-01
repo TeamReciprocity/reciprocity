c2f40ced0af3e7e896c53973bfcf00ac3695ff39
Michael Stokley <michael@michaelstokley.com>
2016-05-01 14:58:49 -0700

    incompatible migrations on recipe model resolved

--- reciprocity/recipe/models.py
+++ reciprocity/recipe/models.py
@@ -8,19 +8,19 @@ TIME_CHOICES = (
     (.25, '15 minutes'),
     (.5, '30 minutes'),
     (.75, '45 minutes'),
-    (1, '1 hour'),
+    (1.0, '1 hour'),
     (1.25, '1 hour 15 minutes'),
     (1.5, '1 hour 30 minutes'),
     (1.75, '1 hour 45 minutes'),
-    (2, '2 hours'),
+    (2.0, '2 hours'),
     (2.25, '2 hours 15 minutes'),
     (2.5, '2 hours 30 minutes'),
     (2.75, '2 hours 45 minutes'),
-    (3, '3 hours'),
+    (3.0, '3 hours'),
     (3.25, '3 hours 15 minutes'),
     (3.5, '3 hours 30 minutes'),
     (3.75, '3 hours 45 minutes'),
-    (4, '4+ hours'),
+    (4.0, '4+ hours'),
 )
 
 
@@ -32,8 +32,8 @@ class Recipe(models.Model):
         """Display model instance."""
         return self.recipe_name
 
-    recipe_name = models.CharField(help_text='What is your recipe called?',
-                                   max_length=128)
+    title = models.CharField(help_text='What is your recipe called?',
+                             max_length=128)
     prep_time = models.FloatField(choices=TIME_CHOICES,
                                   blank=True,
                                   null=True,)
