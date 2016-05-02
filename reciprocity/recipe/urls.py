3b5cd0ec095c08093209912d013e7955b22071d7
wohlfea <wohlfea@gmail.com>
2016-05-02 10:25:31 -0700

    Creation of new ingredient is now available through autocomplete.

--- reciprocity/recipe/urls.py
+++ reciprocity/recipe/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url
-from recipe.views import IngredientAutocomplete
+from recipe.views import IngredientAutocomplete, Ingredient
 
 urlpatterns = [
     url(
         r'ingredient-autocomplete/$',
-        IngredientAutocomplete.as_view(),
+        IngredientAutocomplete.as_view(model=Ingredient, create_field='name'),
         name='ingredient-autocomplete',
     ),
 ]
