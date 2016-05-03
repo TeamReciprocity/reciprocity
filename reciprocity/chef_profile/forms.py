f53b3f1fdac62b2645db3b1dd625ff32aca874bc
Hannah Krager <hannahkrager@gmail.com>
2016-05-03 11:02:15 -0700

    save changes before merging in another branch.

---
+++ reciprocity/chef_profile/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from chef_profile.models import ChefProfile
+from django.contrib.auth.models import User
+
+
+class UserForm(forms.ModelForm):
+    """Modifying limited fields on the User model."""
+
+    class Meta:
+        """Establish Model and fields for user form."""
+
+        model = User
+        fields = ['first_name', 'last_name', 'email']
+
+
+class ChefProfileForm(forms.ModelForm):
+    """Editable Profile for the user."""
+
+    class Meta:
+        """Establish Model and fields for user form."""
+
+        model = ChefProfile
+        exclude = ['user']
--- reciprocity/chef_profile/urls.py
+++ reciprocity/chef_profile/urls.py
@@ -1,8 +1,9 @@
 """Set URLs for profile specific views."""
 from django.conf.urls import url
 from django.views.generic import TemplateView
-from .views import ProfileView
+from .views import ProfileView, edit_profile
 
 urlpatterns = [
-    url(r'^$', ProfileView.as_view(), name='profile',),
+    url(r'^$', ProfileView.as_view(), name='profile'),
+    url(r'edit/$', edit_profile, name='edit_profile')
 ]
