48bec1e3e9f406b3ce8b0f3fda42c3fe7202ea8f
Michael Stokley <michael@michaelstokley.com>
2016-05-04 16:05:43 -0700

    home privacy written, tested

--- reciprocity/reciprocity/tests.py
+++ reciprocity/reciprocity/tests.py
@@ -1,3 +1,20 @@
-from recipe.tests import TestView
+from django.test import Client, TestCase
+from recipe.tests import RecipeFactory, UserFactory
 
-class ViewHome(TestView):
+
+class ViewHome(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = UserFactory()
+        self.public_recipe = RecipeFactory(author=self.user, privacy='pu')
+        self.private_recipe = RecipeFactory(author=self.user, privacy='pr')
+
+    def test_including(self):
+        """Confirm view lists user's recipes."""
+        response = self.client.get('/')
+        self.assertIn(str(self.public_recipe), response.content)
+
+    def test_excluding(self):
+        """Confirm view excludes non-authored recipes."""
+        response = self.client.get('/')
+        self.assertNotIn(str(self.private_recipe), response.content)
