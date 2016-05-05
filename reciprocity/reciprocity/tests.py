0fe3afd237ce60e8aa786c022a6371b0440100cb
Michael Stokley <michael@michaelstokley.com>
2016-05-05 14:12:05 -0700

    tox passing

--- reciprocity/reciprocity/tests.py
+++ reciprocity/reciprocity/tests.py
@@ -12,9 +12,9 @@ class ViewHome(TestCase):
     def test_including(self):
         """Confirm view lists user's recipes."""
         response = self.client.get('/')
-        self.assertIn(str(self.public_recipe), response.content)
+        self.assertIn(str(self.public_recipe), str(response.content))
 
     def test_excluding(self):
         """Confirm view excludes non-authored recipes."""
         response = self.client.get('/')
-        self.assertNotIn(str(self.private_recipe), response.content)
+        self.assertNotIn(str(self.private_recipe), str(response.content))
