b3b6c49ee15d57533ae5ec947c22a63cd6bc2356
Hannah Krager <hannahkrager@gmail.com>
2016-05-26 16:14:01 -0700

    update domain name in allowed hosts.

--- reciprocity/reciprocity/production.py
+++ reciprocity/reciprocity/production.py
@@ -3,6 +3,6 @@ from reciprocity.settings import *
 
 DEBUG = False
 ALLOWED_HOSTS.append('.us-west-2.compute.amazonaws.com',
-                     'reciprocity.sites', '52.39.90.167')
+                     'reciprocity.site', '52.39.90.167')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = ()
