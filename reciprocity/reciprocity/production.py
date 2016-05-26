a7e1061ecf2b7dfd5593d4ab27b6930762ccae8e
Hannah Krager <hannahkrager@gmail.com>
2016-05-26 16:19:49 -0700

    getting ansible error, try list for append.

--- reciprocity/reciprocity/production.py
+++ reciprocity/reciprocity/production.py
@@ -2,7 +2,7 @@
 from reciprocity.settings import *
 
 DEBUG = False
-ALLOWED_HOSTS.append('.us-west-2.compute.amazonaws.com',
-                     'reciprocity.site', '52.39.90.167')
+ALLOWED_HOSTS.append(['.us-west-2.compute.amazonaws.com',
+                     'reciprocity.site', '52.39.90.167'])
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = ()
