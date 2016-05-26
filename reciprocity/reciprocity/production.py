1494043e9f7b989d3a231b387c4da48a3b9f2248
Hannah Krager <hannahkrager@gmail.com>
2016-05-26 16:29:27 -0700

    debug server.

--- reciprocity/reciprocity/production.py
+++ reciprocity/reciprocity/production.py
@@ -1,7 +1,7 @@
 """Overwrite and add settings specifically for production deployed instance."""
 from reciprocity.settings import *
 
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS.append(['.us-west-2.compute.amazonaws.com',
                      'reciprocity.site', '52.39.90.167'])
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
