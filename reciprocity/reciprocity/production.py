bd42114b77b11d59423acbb3726c95c030c7fd59
Hannah Krager <hannahkrager@gmail.com>
2016-05-26 17:13:39 -0700

    confirmed debug=True makes domain functional.

--- reciprocity/reciprocity/production.py
+++ reciprocity/reciprocity/production.py
@@ -1,7 +1,7 @@
 """Overwrite and add settings specifically for production deployed instance."""
 from reciprocity.settings import *
 
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS.append('.us-west-2.compute.amazonaws.com')
 ALLOWED_HOSTS.append('reciprocity.site')
 ALLOWED_HOSTS.append('52.39.90.167')
