57e8d4164559e07ae0dfd61a342b845e04e78fd1
Hannah Krager <hannahkrager@gmail.com>
2016-05-26 17:04:51 -0700

    change append technique.

--- reciprocity/reciprocity/production.py
+++ reciprocity/reciprocity/production.py
@@ -2,7 +2,9 @@
 from reciprocity.settings import *
 
 DEBUG = False
-ALLOWED_HOSTS.append(['.us-west-2.compute.amazonaws.com',
-                     'reciprocity.site', '52.39.90.167'])
+ALLOWED_HOSTS.append('.us-west-2.compute.amazonaws.com')
+ALLOWED_HOSTS.append('reciprocity.site')
+ALLOWED_HOSTS.append('52.39.90.167')
+ALLOWED_HOSTS.append('localhost')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = ()
